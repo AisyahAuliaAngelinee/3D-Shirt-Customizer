6136dce5c0ca989515cc88f7b52b42b98e019f34
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-05-12 15:29:38 +0700

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
+import Home from "./pages/Home.jsx";
+import Customizer from "./pages/Customizer.jsx";
+import Canvas from "./canvas";
+
 function App() {
 	return (
 		<>
 			<main className="app transition-all ease-in">
 				<Home />
+				<Canvas />
+				<Customizer />
 			</main>
 		</>
 	);
